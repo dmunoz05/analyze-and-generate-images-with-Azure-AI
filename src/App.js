654b6eb48d2dcf4f0f8a7a1e8c09de10c910c99b
= <daniel20025febrero29@gmail.com>
2023-11-18 21:42:53 -0500

    Example commit for Azure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value} this is a example</div>;
 }
 
 export default App;
