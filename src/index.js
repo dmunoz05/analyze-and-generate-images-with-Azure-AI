141f0617aa579e5871acd75e6b7dd6c51a700a76
= <daniel20025febrero29@gmail.com>
2023-11-18 23:54:47 -0500

    Reestructure files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '../src/app/App';
+import App from './App';
 import FooterLayout from './layout/footer';
 
 ReactDOM.render(<> <App /> <FooterLayout /> </> , document.getElementById('root'));
