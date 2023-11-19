6924ce49812958d4745765a12210ca1ff1422464
= <daniel20025febrero29@gmail.com>
2023-11-18 23:58:58 -0500

    Return json

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import './App.css';
+import FooterLayout from './footer';
 
 function App() {
   const value = 'World';
   return (
-    <div className='flex flex-col items-center w-full gap-7'>
-      <h1 className='font-bold'>Computer Vision</h1>
+    <>
+      <div className='flex flex-col items-center w-full gap-7'>
+        <h1 className='font-bold'>Computer Vision</h1>
 
-      <p>This is a app for generate images using Azure AI.</p>
+        <p>This is a app for generate images using Azure AI.</p>
 
 
-      <br />
-      <input type="text" name="text" className="input" placeholder="Enter URL to analyzate or textual prompt to genera at image" />
+        <br />
+        <input type="text" name="text" className="input" placeholder="Enter URL to analyzate or textual prompt to genera at image" />
 
-      <div className='flex flex-row justify-center items-center gap-5'>
-        <button className="btn">
-          <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
-            <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
-          </svg>
+        <div className='flex flex-row justify-center items-center gap-5'>
+          <button className="btn">
+            <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
+              <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+            </svg>
 
-          <span className="text">Analyzate</span>
-        </button>
+            <span className="text">Analyzate</span>
+          </button>
 
-        <button className="btn">
-          <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
-            <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
-          </svg>
+          <button className="btn">
+            <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
+              <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+            </svg>
 
-          <span className="text">Generate</span>
-        </button>
+            <span className="text">Generate</span>
+          </button>
+        </div>
       </div>
-    </div>
+      <FooterLayout />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import FooterLayout from './layout/footer';
-
-ReactDOM.render(<> <App /> <FooterLayout /> </> , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
