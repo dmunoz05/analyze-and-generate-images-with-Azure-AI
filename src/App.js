af8262c73d7b428db8b70c77903da18924544529
= <daniel20025febrero29@gmail.com>
2023-11-19 02:02:35 -0500

    Add Analyzate Image function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 import FooterLayout from './footer';
 
 function App() {
+
+
+  function analyzateImage() {
+    debugger;
+    var url = document.getElementsByName("text")[0].value;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.aitextgen.app/generate?apiKey=YOUR_API_KEY&text=' + url, true);
+    xhr.send();
+    xhr.onload = function () {
+      var image = document.getElementById("image");
+      image.src = JSON.parse(xhr.response).data[0].image;
+    }
+  }
   
   return (
     <>
@@ -13,25 +26,27 @@ function App() {
 
 
         <br />
-        <input type="text" name="text" className="input" placeholder="Enter URL to analyzate or textual prompt to genera at image" />
+        <input type='text' name='text' className='input' placeholder='Enter URL to analyzate or textual prompt to genera at image' />
 
         <div className='flex flex-row justify-center items-center gap-5'>
-          <button className="btn">
-            <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
-              <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+          <button onClick={analyzateImage} className="btn">
+            <svg height='24' width='24' fill='#FFFFFF' viewBox='0 0 24 24' data-name='Layer 1' id='Layer_1' className='sparkle'>
+              <path d='M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z'></path>
             </svg>
 
-            <span className="text">Analyzate</span>
+            <span className='text'>Analyzate</span>
           </button>
 
-          <button className="btn">
-            <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
-              <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+          <button className='btn'>
+            <svg height='24' width='24' fill='#FFFFFF' viewBox='0 0 24 24' data-name='Layer 1' id='Layer_1' className='sparkle'>
+              <path d='M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z'></path>
             </svg>
 
-            <span className="text">Generate</span>
+            <span className='text'>Generate</span>
           </button>
         </div>
+
+        <img alt='' src='' id='image' className='w-full' />
       </div>
       <FooterLayout />
     </>
