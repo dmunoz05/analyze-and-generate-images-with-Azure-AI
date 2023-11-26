5ddb960fb750a59c1185bb0ccc619c77f665281f
= <daniel20025febrero29@gmail.com>
2023-11-26 03:45:07 -0500

    Change name app and css footer

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FooterLayout() {
     return (
-        <div className='w-auto flex flex-row justify-center p-2 absolute bottom-0 left-0 right-0 bg-slate-900'>
+        <div className='w-auto flex flex-row justify-center p-2 fixed bottom-0 left-0 right-0 bg-slate-900'>
             <p className="read-the-docs">
                 Created in <code><a target='__blank' href='https://reactjs.org/'>React</a></code> by <code><a target='__blank' href='https://github.com/dmunoz05'>Daniel Muñoz</a></code> from <code><a target='__blank' href='https://github.com/dmunoz05?tab=repositories'>dmunoz05</a></code>
             </p>
